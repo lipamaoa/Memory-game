f31f29ef25d78cfadffa4ec4e9acfc647507c001
lipamaoa <filipa.ferreirasilva@gmail.com>
2023-09-18 16:13:04 +0100

    added game logic

--- src/index.js
+++ src/index.js
@@ -25,28 +25,38 @@ const cards = [
   { name: 'thor', img: 'thor.jpg' }
 ];
 
-const memoryGame = new MemoryGame(cards);
+const numPlayers=4;
+
+const memoryGame = new MemoryGame(cards, numPlayers);
 let selectedCards = [];
-let pairsClicked = null;
-let pairsGuessed = null;
+let currentPlayerIndex = 1;
 
 window.addEventListener('load', (event) => {
   memoryGame.shuffleCards();
   let html = '';
   memoryGame.cards.forEach((pic) => {
     html += `
-      <div class="card" data-card-name="${pic.name}">
-        <div class="back" name="${pic.img}"></div>
-        <div class="front" style="background: url(img/${pic.img}) no-repeat"></div>
-      </div>
+    <div class="card" data-card-name="${pic.name}">
+    <div class="back" name="${pic.img}"></div>
+    <div class="front" style="background: url(img/${pic.img}) no-repeat"></div>
+    </div>
     `;
   });
-
+  
   // Add all the divs to the HTML
   document.querySelector('#memory-board').innerHTML = html;
-
-  pairsGuessed = document.querySelector('#pairs-guessed');
-  pairsClicked = document.querySelector('#pairs-clicked');
+  
+  let div ='';
+  memoryGame.players.forEach((player)=>{
+    div +=`<div class="player${player.id}-score">
+    <h2>Player ${player.id}</h2>
+    <p>Pairs clicked: <span id="player${player.id}-pairs-clicked">0</span></p>
+    <p>Pairs guessed: <span id="player${player.id}-pairs-guessed">0</span></p>
+    </div>`
+  })
+  
+  document.querySelector('.score-container').innerHTML= div;
+  document.querySelector(`.player${currentPlayerIndex}-score`).classList.toggle("active-player");
 
   // Bind the click event of each element to a function
   document.querySelectorAll('.card').forEach((card) => {
@@ -57,23 +67,37 @@ window.addEventListener('load', (event) => {
       if (selectedCards.length == 2) {
         return;
       }
-
+      
+      
       card.classList.toggle("turned");
-      selectedCards.push(card);
 
+      selectedCards.push(card);
+      
       if (selectedCards.length == 2) {
         const firstCardName = selectedCards[0].getAttribute('data-card-name');
         const secondCardName = selectedCards[1].getAttribute('data-card-name');
-
+        
         const isMatch = memoryGame.checkIfPair(firstCardName, secondCardName);
 
-        pairsGuessed.textContent = memoryGame.pairsGuessed;
-        pairsClicked.textContent = memoryGame.pairsClicked;
+        
+        pairsGuessed = document.querySelector(`#player${currentPlayerIndex}-pairs-guessed`);
+        pairsClicked = document.querySelector(`#player${currentPlayerIndex}-pairs-clicked`);
+        
+        let currentPlayer = memoryGame.getCurrentPlayer();
+        pairsGuessed.textContent = currentPlayer.pairsGuessed;
+        pairsClicked.textContent = currentPlayer.pairsClicked;
+
+        if(!isMatch){
+          document.querySelector(`.player${currentPlayerIndex}-score`).classList.toggle("active-player");
+          memoryGame.switchPlayer();
+          currentPlayerIndex = memoryGame.getCurrentPlayer().id;
+          console.log(document.querySelector(`.player${currentPlayerIndex}-score`).classList.toggle("active-player"));
+        }
 
         setTimeout(() => {
           if (isMatch) {
             if (memoryGame.checkIfFinished()) {
-              alert(`You Win!!! Took you ${memoryGame.pairsClicked} clicks to find ${memoryGame.pairsGuessed} guesses.`);
+              alert(`Player ${currentPlayerIndex} Wins!!! Took you ${memoryGame.players[currentPlayerIndex].pairsClicked} clicks to find ${memoryGame.players[currentPlayerIndex].pairsGuessed} guesses.`);
             }
           }
           else {
@@ -81,6 +105,8 @@ window.addEventListener('load', (event) => {
             selectedCards[1].classList.toggle("turned");
           }
           selectedCards = [];
+
+          
         }, 500);
       }
     });
